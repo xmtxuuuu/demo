679ed4a7e454a7ba5927bfc574cfd5c9ee404076
yms <1174846372@qq.com>
2020-09-19 15:39:31 +0800

    注释

---
+++ Demo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Demo
+//
+//  Created by yms on 9/18/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
